8c012ffbb140d59ad0157c12930815700e76d508
NICHOLAS1076 <nicholasniles0@gmail.com>
2022-10-30 17:06:55 -0400

    v1.3 - Visual Updates

--- Unity/Scripts/SensorText.cs
+++ Unity/Scripts/SensorText.cs
@@ -16,6 +16,17 @@ public class SensorText : MonoBehaviour
     public SelectionMenu selMen;
     //SerialPort stream = new SerialPort("COM4", 9600);
 
+    //Electron Visual
+    public SpriteRenderer electronChannel;
+    public SpriteRenderer electronChannelBorder;
+    public ParticleSystem electrons;
+
+    //Source Visual
+    public GameObject AA;
+    public GameObject nineVolt;
+    public GameObject missing;
+    public GameObject unknown;
+
     private void Start()
     {
         //stream.Close();
@@ -26,6 +37,38 @@ public class SensorText : MonoBehaviour
     void Update()
     {
         UpdateSensorText();
+    }
+
+    public void DisplaySource(int id)
+    {
+        if (id == 0)
+        {
+            missing.gameObject.SetActive(true);
+            nineVolt.gameObject.SetActive(false);
+            AA.gameObject.SetActive(false);
+            unknown.gameObject.SetActive(false);
+        }
+        else if (id == 1)
+        {
+            missing.gameObject.SetActive(false);
+            nineVolt.gameObject.SetActive(false);
+            AA.gameObject.SetActive(true);
+            unknown.gameObject.SetActive(false);
+        }
+        else if (id == 2)
+        {
+            missing.gameObject.SetActive(false);
+            nineVolt.gameObject.SetActive(true);
+            AA.gameObject.SetActive(false);
+            unknown.gameObject.SetActive(false);
+        }
+        else if (id == 3)
+        {
+            missing.gameObject.SetActive(false);
+            nineVolt.gameObject.SetActive(false);
+            AA.gameObject.SetActive(false);
+            unknown.gameObject.SetActive(true);
+        }
     }
 
     IEnumerator ReadSerial()
@@ -115,6 +158,36 @@ public class SensorText : MonoBehaviour
     public void Update_MoistureVisual(float reading)
     {
         sensorText.text = reading.ToString() + "v";
+        electronChannel.transform.localScale = new Vector3(electronChannel.transform.localScale.x, reading, electronChannel.transform.localScale.z);
+        electronChannelBorder.transform.localScale = new Vector3(electronChannelBorder.transform.localScale.x, reading + 0.45f, electronChannelBorder.transform.localScale.z);
+
+        if (reading <= 0)
+        {
+            electrons.Stop();
+        }
+        else
+        {
+            electrons.Play();
+            var sh = electrons.shape;
+            sh.radius = reading / 6.1f;
+        }
+
+        if (reading <= 0)
+        {
+            DisplaySource(0);
+        }
+        else if (reading > 1 && reading < 2)
+        {
+            DisplaySource(1);
+        }
+        else if (reading > 8 && reading < 11)
+        {
+            DisplaySource(2);
+        }
+        else
+        {
+            DisplaySource(3);
+        }
     }
 
 }
